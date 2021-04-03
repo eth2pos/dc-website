a05e0f141bda34d6fe9cd5cd673d26cd02f550a9
eth2pos <eth2pos@gmail.com>
2021-04-03 22:52:40 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+USDG landing page
---
+++ src/Home/data.source.js
@@ -0,0 +1,279 @@
+import React from 'react';
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo kn1te2uesnt-editor_css',
+    children: 'https://i.imgur.com/wZKZpkC.png',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item0',
+        className: 'header0-item kn1qg0ma4m-editor_css',
+        children: {
+          href: '#',
+          children: [{ children: '导航一', name: 'text' }],
+        },
+        subItem: [
+          {
+            name: 'sub0',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+          {
+            name: 'sub1',
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item',
+              children: [
+                {
+                  name: 'image0',
+                  className: 'item-image',
+                  children:
+                    'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg',
+                },
+                {
+                  name: 'title',
+                  className: 'item-title',
+                  children: 'Ant Design',
+                },
+                {
+                  name: 'content',
+                  className: 'item-content',
+                  children: '企业级 UI 设计体系',
+                },
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Banner50DataSource = {
+  wrapper: { className: 'home-page-wrapper banner5' },
+  page: { className: 'home-page banner5-page kn1ukihmx4-editor_css' },
+  childWrapper: {
+    className: 'banner5-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>DCoin</p>
+          </span>
+        ),
+        className: 'banner5-title',
+      },
+      {
+        name: 'explain',
+        className: 'banner5-explain',
+        children: (
+          <span>
+            <span>
+              <p>DCoin，一个关于疯涨的传奇故事</p>
+            </span>
+          </span>
+        ),
+      },
+      {
+        name: 'content',
+        className: 'banner5-content',
+        children: (
+          <span>
+            <p>
+              <br />
+            </p>
+          </span>
+        ),
+      },
+      {
+        name: 'button',
+        className: 'banner5-button-wrapper',
+        children: {
+          href: '#',
+          className: 'banner5-button kn1uir12fig-editor_css',
+          type: 'primary',
+          children: '开始使用',
+        },
+      },
+    ],
+  },
+  image: {
+    className: 'banner5-image kn1uixearc-editor_css',
+    children: 'https://i.imgur.com/ZRuFacg.png',
+  },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>Dcoin产出机制</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>减产机制</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>
+                    通兑塔每上升4层/Dcoin（DC）出币量减产一半（每次减半都将引发币价疯狂上涨）
+                  </p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>销毁机制</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <span>
+                    <p>
+                      Dcoin每当交易和转账一次，其总量销毁3%，代币总量每销毁3%其价格自动上升6%
+                    </p>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>挖矿</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>
+                    <span>
+                      通过DC➕ETH可以获得永久算力，生产（Dtoken）所有参与挖矿的DC➕ETH都会被销毁，所以DC和ETH都将因此暴涨！
+                    </span>
+                    <br />
+                  </p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <p>
+          <span>©2021 DCoin&nbsp;All Rights Reserved</span>
+        </p>
+      </span>
+    ),
+  },
+};
